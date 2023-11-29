5d309b191162bfb8293c8384e01368e94bde1b68
magnet <magnet7777@yandex.ru>
2023-11-29 13:40:17 +0300

    lab

--- calculator/mainwindow.h
+++ calculator/mainwindow.h
@@ -17,19 +17,46 @@ public:
 
 private slots:
     void on_btn_clear_clicked();
+
     void on_btn_0_clicked();
 
     void on_btn_1_clicked();
 
+    void on_btn_2_clicked();
+
+    void on_btn_3_clicked();
+
+    void on_btn_4_clicked();
+
+    void on_btn_5_clicked();
+
+    void on_btn_6_clicked();
+
+    void on_btn_7_clicked();
+
+    void on_btn_8_clicked();
+
+    void on_btn_9_clicked();
+
     void on_btn_add_clicked();
 
+    void on_btn_neg_clicked();
+
+    void on_btn_sub_clicked();
+
+    void on_btn_mul_clicked();
+
+    void on_btn_div_clicked();
+
     void on_btn_point_clicked();
+
+    void on_btn_calc_clicked();
 
     void on_btn_ce_clicked();
 
     void on_btn_backspace_clicked();
 
-    void on_btn_neg_clicked();
+    QString calculateExpression(const QString &expression);
 
 private:
     Ui::MainWindow *ui;
